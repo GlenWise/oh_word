d715b6750ba5dba8106440e14b0119f574a32959
Glen Wise <gwise@andrew.cmu.edu>
2016-11-20 17:50:44 -0500

    Initial Commit

---
+++ Oh, Word?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oh, Word?
+//
+//  Created by Glen Wise on 11/20/16.
+//  Copyright © 2016 FishbulbApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
